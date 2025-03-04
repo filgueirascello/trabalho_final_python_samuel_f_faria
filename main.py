408ac831a0217d0d4debdb383adb886b843b97b9
samuelfilgueiras <filgueirascello@gmail.com>
2025-03-04 00:20:03 -0300

    escrita no banco

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pandas as pd
+import sqlite3
 from transform import classifica_turno, calc_horas
 from data_clean import data_clean
 
+# Conectando ao banco SQLite (o arquivo será criado se não existir)
+conn = sqlite3.connect('trabalhofinal.db')
 
 
 # Gera o DataFrame a partir do clean.py
 df = data_clean()
 
 
-
 df['tempo_voo_minutos'] = calc_horas(df['tempo_voo'])
-
 
 
 df['turno'] = df['data_hora'].apply(classifica_turno)
 
 
-df.head()
+# df.head()
 
-print('finalizei')
+# print('finalizei')
 
-df.to_csv("base_final.csv", index=False)
+# df.to_csv("base_final.csv", index=False)
+
+
+# Inserindo o DataFrame na tabela
+df.to_sql('table_trabalho_final', conn, if_exists='replace', index=False)
