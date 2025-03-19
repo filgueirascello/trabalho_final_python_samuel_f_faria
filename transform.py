422978f9a6c27d762065b1b8298d4ed67124d8be
samuelfilgueiras <filgueirascello@gmail.com>
2025-03-18 21:14:50 -0300

    comentários

--- transform.py
+++ transform.py
@@ -1,13 +1,20 @@
 import pandas as pd
 
 def calc_horas(coluna_tempo_voo):
+    """"
+    
+    Função para criar coluna_tempo_voo em minutos
+    
+    """
     return coluna_tempo_voo * 60
 
 
 def classifica_turno(coluna_data_hora):
-    hora = coluna_data_hora.hour  # Extrai a hora da coluna datetime
+    hora = coluna_data_hora.hour  
+    """
+    Classificação com base nas faixas horárias
+    """
     
-    # Classificação com base nas faixas horárias
     if 6 <= hora < 12:
         return "MANHÃ"
     elif 12 <= hora < 18:
